2c6b6a9e24608d999c0f1fa946d52e01b62953ac
Faaiq Ali Khan <50907582+FaaiqKhan@users.noreply.github.com>
2023-07-11 14:57:58 +0500

    model created mainActivity updated and drawable added

---
+++ app/src/main/java/com/practice/amphibians/models/Amphibian.kt
@@ -0,0 +1,13 @@
+package com.practice.amphibians.models
+
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class Amphibian(
+    val name: String,
+    val type: String,
+    val description: String,
+    @SerialName(value = "img_src")
+    val image: String,
+)
